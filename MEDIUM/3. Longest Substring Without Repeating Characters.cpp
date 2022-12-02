c7cdc12d7e0a1bf966d4f2ce8fd70bb9b8074e3b
Ritam Chakraborty <104848386+0LEUM@users.noreply.github.com>
2022-12-02 21:31:27 +0530

    Create 3. Longest Substring Without Repeating Characters.cpp

---
+++ MEDIUM/3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,60 @@
+/*Given a string s, find the length of the longest 
+substring
+ without repeating characters.
+
+ 
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if(s.length()==0)return 0;   //if string of length zero comes simply return 0
+        unordered_map<char,int> m;   //create map to store frequency,(get to know all unique characters
+        int i=0,j=0,ans=INT_MIN; 
+        while(j<s.length())   
+        {
+            m[s[j]]++;  //increase the frequency of the element as you traverse the string
+            if(m.size()==j-i+1)  // whem map size is equal to the window size means suppose window size is 3 and map size is also three that means in map all unique characters are their
+            {
+                ans = max(ans,j-i+1);  //compare the length of the maximum window size
+            }
+            else if(m.size()<j-i+1)   //if the map size is less than the window size means there is some duplicate present like window size = 3 and map size = 2 means there is a duplicates
+            {
+                while(m.size()<j-i+1)  //so till the duplicates are removed completely
+                {
+                    m[s[i]]--;   //remove the duplicates
+                    if(m[s[i]]==0)  //if the frequency becomes zero 
+                    {
+                        m.erase(s[i]);//delete it completely
+                    }
+                    i++;  //go for next element 
+                }
+            }
+             j++;  //go for the next element
+        }
+        return ans;
+    }
+};

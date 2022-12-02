2be95a0452d07acb2c7e7453564953ec000b913a
Ritam Chakraborty <104848386+0LEUM@users.noreply.github.com>
2022-12-02 22:05:15 +0530

    Create 990. Satisfiability of Equality Equations.cpp

---
+++ MEDIUM/990. Satisfiability of Equality Equations.cpp
@@ -0,0 +1,45 @@
+/*You are given an array of strings equations that represent relationships between variables where each string equations[i] is of length 4 and takes one of two different forms: "xi==yi" or "xi!=yi".Here, xi and yi are lowercase letters (not necessarily different) that represent one-letter variable names.
+
+Return true if it is possible to assign integers to variable names so as to satisfy all the given equations, or false otherwise.
+
+ 
+Example 1:
+
+Input: equations = ["a==b","b!=a"]
+Output: false
+Explanation: If we assign say, a = 1 and b = 1, then the first equation is satisfied, but not the second.
+There is no way to assign the variables to satisfy both equations.
+
+Example 2:
+
+Input: equations = ["b==a","a==b"]
+Output: true
+Explanation: We could assign a = 1 and b = 1 to satisfy both equations.
+ 
+
+Constraints:
+
+1 <= equations.length <= 500
+equations[i].length == 4
+equations[i][0] is a lowercase letter.
+equations[i][1] is either '=' or '!'.
+equations[i][2] is '='.
+equations[i][3] is a lowercase letter.*/
+
+class Solution {
+    int uf[26];
+    int find(int x) {
+        return uf[x] == x ? x : (uf[x] = find(uf[x]));
+    }
+public:
+    bool equationsPossible(vector<string>& equations) {
+        for (int i = 0; i < 26; ++i) uf[i] = i;
+        for (auto e : equations) {
+            if (e[1] == '=') uf[find(e[0] - 'a')] = find(e[3] - 'a'); 
+        }
+        for (auto e : equations) {
+            if (e[1] == '!' && find(e[0] - 'a') == find(e[3] - 'a')) return false;
+        }
+        return true;
+    }
+};

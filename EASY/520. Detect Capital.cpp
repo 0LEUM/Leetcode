94933d5a931d9dc307791fd4bec0caecab58da0c
Ritam Chakraborty <104848386+0LEUM@users.noreply.github.com>
2023-01-02 12:16:11 +0530

    Create 520. Detect Capital.cpp

---
+++ EASY/520. Detect Capital.cpp
@@ -0,0 +1,43 @@
+/*We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.
+
+ 
+Example 1:
+
+Input: word = "USA"
+Output: true
+
+Example 2:
+
+Input: word = "FlaG"
+Output: false
+ 
+
+Constraints:
+
+1 <= word.length <= 100
+word consists of lowercase and uppercase English letters.*/
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count=0;
+        if(word.size()==1)
+            return true;
+        
+        for(int i=0; i<word.size(); i++)
+            if(isupper(word[i]))
+                count++;
+        
+        if(count==1 && isupper(word[0]))
+            return true;
+        if(count==0 || count==word.size())
+            return true;
+        else
+            return false;
+    }
+};

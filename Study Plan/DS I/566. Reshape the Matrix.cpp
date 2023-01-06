866ab4392b17b238e9c471ae6251290c782f4207
Ritam Chakraborty <104848386+0LEUM@users.noreply.github.com>
2023-01-06 11:21:31 +0530

    Create 566. Reshape the Matrix.cpp

---
+++ Study Plan/DS I/566. Reshape the Matrix.cpp
@@ -0,0 +1,44 @@
+/*In MATLAB, there is a handy function called reshape which can reshape an m x n matrix into a new one with a different size r x c keeping its original data.
+
+You are given an m x n matrix mat and two integers r and c representing the number of rows and the number of columns of the wanted reshaped matrix.
+
+The reshaped matrix should be filled with all the elements of the original matrix in the same row-traversing order as they were.
+
+If the reshape operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+
+ 
+Example 1:
+
+Input: mat = [[1,2],[3,4]], r = 1, c = 4
+Output: [[1,2,3,4]]
+
+Example 2:
+
+Input: mat = [[1,2],[3,4]], r = 2, c = 4
+Output: [[1,2],[3,4]]
+ 
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 100
+-1000 <= mat[i][j] <= 1000
+1 <= r, c <= 300*/
+
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+        int m = nums.size(), n = nums[0].size();
+        if(m * n != r * c) return nums;
+        vector<vector<int>> ans(r, vector<int>(c));
+        int row = 0, col = 0;
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                ans[i][j] = nums[row][col++];
+                if(col == n) col = 0, row++;
+            }
+        }
+        return ans;
+    }
+};

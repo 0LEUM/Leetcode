9b1a91c89720274f3bc0417bff8cc1e224020208
Ritam Chakraborty <104848386+0LEUM@users.noreply.github.com>
2023-01-09 20:54:27 +0530

    Create 21. Merge Two Sorted Lists.cpp

---
+++ Study Plan/DS I/21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,73 @@
+/*You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+
+Example 1:
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+  {
+		// if list1 happen to be NULL
+		// we will simply return list2.
+		if(l1 == NULL)
+        {
+			return l2;
+		}
+		
+		// if list2 happen to be NULL
+		// we will simply return list1.
+		if(l2 == NULL)
+        {
+			return l1;
+		} 
+		
+		// if value pointend by l1 pointer is less than equal to value pointed by l2 pointer
+		// we wall call recursively l1 -> next and whole l2 list.
+		if(l1 -> val <= l2 -> val)
+        {
+			l1 -> next = mergeTwoLists(l1 -> next, l2);
+			return l1;
+		}
+		// we will call recursive l1 whole list and l2 -> next
+		else
+        {
+			l2 -> next = mergeTwoLists(l1, l2 -> next);
+			return l2;            
+		}
+	}
+};	

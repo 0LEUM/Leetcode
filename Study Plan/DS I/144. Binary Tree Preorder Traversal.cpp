14e8b18879094a56cb16c766aff944fe58019ea5
Ritam Chakraborty <104848386+0LEUM@users.noreply.github.com>
2023-01-12 21:33:28 +0530

    Create 144. Binary Tree Preorder Traversal.cpp

---
+++ Study Plan/DS I/144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,36 @@
+/*Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+Example 1:
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+
+Example 2:
+
+Input: root = []
+Output: []
+
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100*/
+
+class Solution {
+public:
+    vector<int>ans;
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root){
+            ans.push_back(root->val);
+            preorderTraversal(root->left);
+            preorderTraversal(root->right);
+        }
+        return ans;
+    }
+};
